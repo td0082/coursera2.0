481cd280a8d0410ce3f5a9f35d3a683152eeb14f
td <takshpatel.201106@gmail.com>
2026-02-12 20:41:43 +0530

    Implement helloSpeaker module with speak function

--- module4-solution/helloSpeaker.js
+++ module4-solution/helloSpeaker.js
@@ -1,1 +1,9 @@
+(function (window) {
+var helloSpeaker = {};
+helloSpeaker.speakWord = "Hello";
+helloSpeaker.speak = function (name) {
+  console.log(helloSpeaker.speakWord + " " + name);
+}
+window.helloSpeaker = helloSpeaker;
+})(window);
 
