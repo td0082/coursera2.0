33a24bfa300cfb5f69b60512bc92be1c17348da8
td <takshpatel.201106@gmail.com>
2026-02-12 20:42:09 +0530

    Implement byeSpeaker module to say goodbye

--- module4-solution/byeSpeaker.js
+++ module4-solution/byeSpeaker.js
@@ -1,1 +1,9 @@
+(function (window) {
+var byeSpeaker = {};
+byeSpeaker.speakWord = "Good Bye";
+byeSpeaker.speak = function (name) {
+  console.log(byeSpeaker.speakWord + " " + name);
+}
+window.byeSpeaker = byeSpeaker;
+})(window);
 
